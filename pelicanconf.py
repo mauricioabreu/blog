59833802a2316b64e9b9b8ac1fb9aea601ea609d
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-11-25 00:03:45 -0200

    Initial Pelican architecture
    Requirements, conf files and a Vagrant setup to local development.

---
+++ pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'maugzoide'
+SITENAME = u'maugzoide'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/mauricioabreu'),
+          ('Twitter', 'https://twitter.com/maugzoide'),)
+
+DEFAULT_PAGINATION = 10
---
+++ publishconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import os
+import sys
+sys.path.append(os.curdir)
+
+from pelicanconf import *
+
+
+SITEURL = 'http://blog.maugzoide.com'
+RELATIVE_URLS = False
+TIMEZONE = 'America/Sao_Paulo'
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
