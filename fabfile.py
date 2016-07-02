fd26730b731606fb55a9514ae2f39ce398655b1f
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2016-07-02 20:25:02 -0300

    I suck at sudoering

--- fabfile.py
+++ fabfile.py
@@ -70,10 +70,6 @@ def preview():
 def publish():
     """Publish to production via rsync"""
     local('pelican -s publishconf.py')
-    # Upload nginx files and reload
-    put('nginx.conf', '/etc/nginx/nginx.conf', mode=0644)
-    put('maugzoide.com.conf', '/etc/nginx/sites-available/maugzoide.com.conf', mode=0644)
-    sudo('service nginx reload')
     # rsync all blog files
     project.rsync_project(
         remote_dir=dest_path,
