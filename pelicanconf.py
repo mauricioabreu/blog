4d7945b74dc98c91aa1b8be9977d5f24f27fcf5a
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2018-05-06 11:19:46 -0300

    fix: Generated URL for links

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,8 +6,8 @@ AUTHOR = u'Mauricio Antunes'
 SITENAME = u'maugzoide'
 SITETITLE = "Mauricio Antune's blog"
 SITESUBTITLE = u'people, technology, culture'
-SITELOGO = u'https://www.maugzoide.com/static/profile.png'
-SITEURL = 'https://www.maugzoide.com'
+SITELOGO = u'../static/profile.png'
+SITEURL = 'http://localhost:8000'
 MAIN_MENU = True
 MENUITEMS = (('Archives', '/archives.html'),
              ('Categories', '/categories.html'),
@@ -38,7 +38,7 @@ PLUGINS = ['assets', 'gzip_cache']
 # Social widget
 SOCIAL = (('github', 'https://github.com/mauricioabreu'),
           ('twitter', 'https://twitter.com/maugzoide'),
-          ('rss', '//blog.maugzoide.com/feeds/all.atom.xml'))
+          ('rss', '//www.maugzoide.com/feeds/all.atom.xml'))
 GOOGLE_ANALYTICS = 'UA-70804647-1'
 DISQUS_SITENAME = 'maugzoide'
 ADD_THIS_ID = 'ra-5695b2206aa8b571'
